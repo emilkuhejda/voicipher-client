779220a57d7a24dc7d3628fd307c355ad77d65ab
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-27 17:28:14 +0100

    Add ngx-translate package.

--- projects/home/src/app/app.module.ts
+++ projects/home/src/app/app.module.ts
@@ -6,19 +6,31 @@ import { AppComponent } from './app.component';
 import { DefaultComponent } from './default/default.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DefaultComponent,
-    AboutComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DefaultComponent,
+        AboutComponent,
+        NotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
