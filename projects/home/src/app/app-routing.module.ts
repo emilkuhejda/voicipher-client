18db3e2830f93faa53d02830b17d81579c494e48
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-27 17:21:23 +0100

    Create components.

--- projects/home/src/app/app-routing.module.ts
+++ projects/home/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DefaultComponent } from './default/default.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: DefaultComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- projects/home/src/app/app.module.ts
+++ projects/home/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
