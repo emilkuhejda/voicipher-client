76a26484db54e763bdca5476f8d1ef3bd02452a9
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-03-28 12:45:31 +0200

    Implementation of the translation. (#6)

--- projects/home/src/app/app.module.ts
+++ projects/home/src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LocalizationComponent } from './localization/localization.component';
+import { AboutComponent } from './localization/about/about.component';
+import { environment } from '@home/environment';
+import { ServiceModule } from './service/service.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DefaultComponent,
+        AboutComponent,
+        NotFoundComponent,
+        LocalizationComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ServiceModule,
+        TranslateModule.forRoot({
+            defaultLanguage: environment.defaultLanguage,
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
