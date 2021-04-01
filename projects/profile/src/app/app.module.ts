df635dd9196f4c1586f1b8fca40134ba52bcb1ae
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-01 08:47:20 +0200

    Install translation module to profile client.

--- projects/profile/src/app/app.module.ts
+++ projects/profile/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent, TopbarComponent, VocComponentsModule } from 'projects/voc-components/src/public-api';
+import { VocComponentsModule } from 'projects/voc-components/src/public-api';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { environment } from '@profile/environment';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -12,7 +16,16 @@ import { SidebarComponent, TopbarComponent, VocComponentsModule } from 'projects
     imports: [
         BrowserModule,
         AppRoutingModule,
-        VocComponentsModule
+        VocComponentsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: environment.defaultLanguage,
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
