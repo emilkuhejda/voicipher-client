8901627b2701f6c77ef6a74e1f92e82860fda944
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-01 08:38:24 +0200

    Create main layout components in library and use them in the profile. (#11)

--- projects/voc-components/src/lib/voc-components.module.ts
+++ projects/voc-components/src/lib/voc-components.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
+import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+const components = [
+    MainComponent,
+    SidebarComponent,
+    TopbarComponent,
+    LogoutDialogComponent
+];
 
 @NgModule({
-    declarations: [],
-    imports: [
-    ],
-    exports: []
+    declarations: components,
+    imports: [],
+    exports: components
 })
 export class VocComponentsModule { }
