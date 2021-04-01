d7f20ed4eb19152fa296506a2fba4b1fe6ea9dad
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-01 08:38:24 +0200

    Create main layout components in library and use them in the profile. (#11)

---
+++ projects/voc-components/src/lib/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'voc-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+    public constructor() { }
+
+}
