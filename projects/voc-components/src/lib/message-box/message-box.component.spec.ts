c2ba432f148d4ad7bf93291ac1f042d32209c131
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-01 15:20:50 +0200

    Fix unit tests.

--- projects/voc-components/src/lib/message-box/message-box.component.spec.ts
+++ projects/voc-components/src/lib/message-box/message-box.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MessageBoxComponent } from './message-box.component';
 
 describe('MessageBoxComponent', () => {
-  let component: MessageBoxComponent;
-  let fixture: ComponentFixture<MessageBoxComponent>;
+    let component: MessageBoxComponent;
+    let fixture: ComponentFixture<MessageBoxComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MessageBoxComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MessageBoxComponent],
+            imports: [RouterTestingModule]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MessageBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MessageBoxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
