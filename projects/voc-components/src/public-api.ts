de3d9cd182a21527d1b8d68f67c0dbb722879304
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-01 15:29:33 +0200

    Implementation of the Topbar component and using it to profile project. (#14)

--- projects/voc-components/src/public-api.ts
+++ projects/voc-components/src/public-api.ts
@@ -7,6 +7,8 @@ export * from './lib/sidebar/sidebar.models';
 export * from './lib/sidebar/sidebar.component';
 export * from './lib/topbar/topbar.component';
 export * from './lib/logout-dialog/logout-dialog.component';
+export * from './lib/message-box/message-box.component';
+export * from './lib/message-box/message.model';
 
 export * from './lib/voc-components.service';
 export * from './lib/voc-components.module';
