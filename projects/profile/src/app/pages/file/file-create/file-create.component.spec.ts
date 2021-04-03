aa4de3e8a0adbf1015e2aa87436800ce5d9c83ea
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-03 19:47:33 +0200

    Fix unit tests.

--- projects/profile/src/app/pages/file/file-create/file-create.component.spec.ts
+++ projects/profile/src/app/pages/file/file-create/file-create.component.spec.ts
@@ -1,25 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateMockPipe } from '../../../tests/translate.mock.pipe';
 
 import { FileCreateComponent } from './file-create.component';
 
 describe('FileCreateComponent', () => {
-  let component: FileCreateComponent;
-  let fixture: ComponentFixture<FileCreateComponent>;
+    let component: FileCreateComponent;
+    let fixture: ComponentFixture<FileCreateComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FileCreateComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FileCreateComponent, TranslateMockPipe]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FileCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FileCreateComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
