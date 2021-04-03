da45e340cdee6ab192ff4440f6a8d8867354e736
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-03 13:22:07 +0200

    Create missing pages.

---
+++ projects/profile/src/app/pages/file/file-detail/file-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-file-detail',
+    templateUrl: './file-detail.component.html',
+    styleUrls: ['./file-detail.component.scss']
+})
+export class FileDetailComponent {
+
+    public constructor() { }
+
+}
---
+++ projects/profile/src/app/pages/file/file-edit/file-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-file-edit',
+    templateUrl: './file-edit.component.html',
+    styleUrls: ['./file-edit.component.scss']
+})
+export class FileEditComponent {
+
+    public constructor() { }
+
+}
