e4e5dfe313a94c78df2e2e4086f597bf0282455e
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-04 15:01:04 +0200

    Fix unit test.

--- projects/profile/src/app/pages/file/file-edit/file-edit.component.spec.ts
+++ projects/profile/src/app/pages/file/file-edit/file-edit.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -13,7 +14,8 @@ describe('FileEditComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [FileEditComponent, TranslateMockPipe],
             imports: [RouterTestingModule],
-            providers: [provideMockStore()]
+            providers: [provideMockStore()],
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
     });
 
