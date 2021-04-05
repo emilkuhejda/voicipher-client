9e37500b065b9bfd264c1eaba6fa05973ac61a4a
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-05 10:05:03 +0200

    Code refactoring.

--- projects/profile/src/app/pages/message/message-overview/message-overview.component.ts
+++ projects/profile/src/app/pages/message/message-overview/message-overview.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-message-overview',
-  templateUrl: './message-overview.component.html',
-  styleUrls: ['./message-overview.component.scss']
+    selector: 'app-message-overview',
+    templateUrl: './message-overview.component.html',
+    styleUrls: ['./message-overview.component.scss']
 })
 export class MessageOverviewComponent {
 
-  public constructor() { }
+    public constructor() { }
 
 }
