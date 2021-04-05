08219f9d4d18c1bf3a6fe45e714daa397fb05c73
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-05 18:03:42 +0200

    Fix unit tests.

--- projects/profile/src/app/components/transcribe-item/transcribe-item.component.spec.ts
+++ projects/profile/src/app/components/transcribe-item/transcribe-item.component.spec.ts
@@ -1,25 +1,30 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { TranslateMockPipe } from '../../tests/translate.mock.pipe';
+import { RoundConfidencePipe } from '../pipes/round-confidence.pipe';
 
 import { TranscribeItemComponent } from './transcribe-item.component';
 
 describe('TranscribeItemComponent', () => {
-  let component: TranscribeItemComponent;
-  let fixture: ComponentFixture<TranscribeItemComponent>;
+    let component: TranscribeItemComponent;
+    let fixture: ComponentFixture<TranscribeItemComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TranscribeItemComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TranscribeItemComponent, TranslateMockPipe, RoundConfidencePipe],
+            providers: [provideMockStore()],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TranscribeItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TranscribeItemComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
