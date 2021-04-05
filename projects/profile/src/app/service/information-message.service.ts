75259a16bbf2e1a621b7fcf11d2b20dd70e07d0f
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-05 13:23:16 +0200

    Store opened message ids in storage.

--- projects/profile/src/app/service/information-message.service.ts
+++ projects/profile/src/app/service/information-message.service.ts
@@ -4,25 +4,23 @@ import { InformationMessage } from '@profile/core/models/information-message';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { RoutingService } from './routing.service';
+import { StorageService } from './storage.service';
 
 @Injectable()
 export class InformationMessageService {
 
-    public constructor(private routingService: RoutingService, private httpClient: HttpClient) { }
+    public constructor(
+        private routingService: RoutingService,
+        private storageService: StorageService,
+        private httpClient: HttpClient) { }
 
     public getAll(): Observable<InformationMessage[]> {
         return this.httpClient
             .get<InformationMessage[]>(this.routingService.getInformationMessagesUrl())
-            .pipe(
-                map(messages => {
-                    for (let message of messages) {
-                        message.dateUpdated = new Date(message.dateUpdated);
-                        message.datePublished = new Date(message.datePublished);
-                    }
-
-                    return messages;
-                })
-            );
+            .pipe(map(messages => messages.map(x => {
+                const openedMessages = this.storageService.getItem<string[]>('opened.messages') ?? [];
+                return this.mapMessage(x, openedMessages);
+            })));
     }
 
     public get(informationMessageId: string): Observable<InformationMessage> {
@@ -34,17 +32,27 @@ export class InformationMessageService {
         params = params.append('informationMessageId', message.id);
         return this.httpClient
             .put<InformationMessage>(this.routingService.getMarkMessageAsOpenedUrl(), null, { params: params })
-            .pipe(
-                map(message => {
-                    message.dateUpdated = new Date(message.dateUpdated);
-                    message.datePublished = new Date(message.datePublished);
-                    return message;
-                })
-            );
+            .pipe(map(message => {
+                const openedMessages = this.storageService.getItem<string[]>('opened.messages') ?? [];
+                return this.mapMessage(message, openedMessages);
+            }));
     }
 
-    markAsOpenedLocally(message: InformationMessage): Observable<InformationMessage> {
+    public markAsOpenedLocally(message: InformationMessage): Observable<InformationMessage> {
+        let openedMessages = this.storageService.getItem<string[]>('opened.messages') ?? [];
+        if (!openedMessages.includes(message.id)) {
+            openedMessages.push(message.id);
+        }
+
+        this.storageService.setItem('opened.messages', openedMessages);
         return of(message);
+    }
+
+    private mapMessage(message: InformationMessage, openedMessages: string[]): InformationMessage {
+        message.dateUpdated = new Date(message.dateUpdated);
+        message.datePublished = new Date(message.datePublished);
+        message.wasOpened = openedMessages.includes(message.id)
+        return message;
     }
 
 }
