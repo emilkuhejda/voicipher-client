5a95a6958a59abb927a0088285a2d3cd9ee54421
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-05 12:11:44 +0200

    Detail message page.

--- projects/profile/src/app/pages/message/message-detail/message-detail.component.ts
+++ projects/profile/src/app/pages/message/message-detail/message-detail.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Language } from '@profile/core/types/language';
+import { LanguageHelper } from '@profile/core/utils/language-helper';
+import { MessagePageAction } from '@profile/state/actions';
+import { AppState } from '@profile/state/app.state';
+import { getCurrentLanguage } from '@profile/state/selectors';
+import { getCurrentMessage } from '@profile/state/selectors/message.selectors';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { MessageViewModel } from '../message-view.model';
 
 @Component({
     selector: 'app-message-detail',
     templateUrl: './message-detail.component.html',
     styleUrls: ['./message-detail.component.scss']
 })
-export class MessageDetailComponent implements OnInit {
+export class MessageDetailComponent implements OnInit, OnDestroy {
+    private destroy$: Subject<void> = new Subject<void>();
 
-    public constructor() { }
+    public message$: Observable<MessageViewModel | undefined> | undefined;
+    public currentLanguage: Language = 'Undefined';
 
-    public ngOnInit(): void { }
+    public constructor(private store: Store<AppState>, private route: ActivatedRoute) { }
+
+    public ngOnInit(): void {
+        const messageId = this.route.snapshot.params.messageId;
+
+        this.store.dispatch(MessagePageAction.loadCurrentMessageRequest({ messageId }));
+        this.store
+            .select(getCurrentLanguage)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(language => this.currentLanguage = LanguageHelper.convertFromString(language));
+        this.message$ = this.store
+            .select(getCurrentMessage)
+            .pipe(
+                takeUntil(this.destroy$),
+                map(message => {
+                    if (message) {
+                        return new MessageViewModel(message, this.currentLanguage)
+                    }
+
+                    return undefined;
+                })
+            );
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.unsubscribe();
+    }
 
 }
--- projects/profile/src/app/pages/message/message-view.model.ts
+++ projects/profile/src/app/pages/message/message-view.model.ts
@@ -1,29 +1,39 @@
 import { InformationMessage } from '@profile/core/models/information-message';
+import { LanguageVersion } from '@profile/core/models/language-version';
 import { Language } from '@profile/core/types/language';
 
 export class MessageViewModel {
     private message: InformationMessage;
-    private language: Language;
+    private languageVersion: LanguageVersion | undefined;
 
     public id: string;
-    public title: string;
     public wasOpened: boolean;
+    public title: string;
+    public description: string;
 
     public constructor(message: InformationMessage, language: Language) {
         this.message = message;
-        this.language = language;
+        this.languageVersion = this.message.languageVersions.find(x => x.languageString === language);
 
         this.id = message.id;
         this.wasOpened = message.wasOpened;
         this.title = this.getTitle();
+        this.description = this.getDescription();
     }
 
     public getTitle(): string {
-        const languageVersion = this.message.languageVersions.find(x => x.languageString === this.language);
-        if (languageVersion == undefined) {
+        if (!this.languageVersion) {
             return '';
         }
 
-        return languageVersion.title;
+        return this.languageVersion.title;
+    }
+
+    public getDescription(): string {
+        if (!this.languageVersion) {
+            return ''
+        }
+
+        return this.languageVersion.description;
     }
 }
