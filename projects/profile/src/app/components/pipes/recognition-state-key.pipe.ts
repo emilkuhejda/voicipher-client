5f1bf88780db4ab12e6b091f6eb231544f1dd850
Emil Kuhejda <emil.kuhejda@gmail.com>
2021-04-08 20:53:03 +0200

    Update files layout.

---
+++ projects/profile/src/app/components/pipes/recognition-state-key.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'recognitionStateKey'
+})
+export class RecognitionStateKeyPipe implements PipeTransform {
+
+    public transform(value: string): any {
+        switch (value) {
+            case 'None':
+                return 'RecognitionState.Ready';
+            case 'Converting':
+            case 'Prepared':
+            case 'InProgress':
+                return 'RecognitionState.InProgress';
+            case 'Completed':
+                return 'RecognitionState.Completed';
+        }
+    }
+
+}
